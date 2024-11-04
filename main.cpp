1807e565952fabde47d1db6df0ebc7fe0813112b
Dan <texhnistguru@gmail.com>
2024-11-03 19:39:07 -0800

    Commit 2

--- main.cpp
+++ main.cpp
@@ -1,18 +1,48 @@
 // COMSC-210 | Lab 27 | Dan Pokhrel
 #include <iostream>
 #include <map>
+#include <string>
 #include <vector>
 #include <tuple>
 using namespace std;
 
 // prototypes
 int menu();
+void add_vil(map<string, tuple<int, string, string>> &villagers);
+void delete_vil(map<string, tuple<int, string, string>> &villagers);
+void increase(map<string, tuple<int, string, string>> &villagers);
+void decrease(map<string, tuple<int, string, string>> &villagers);
+void search(map<string, tuple<int, string, string>> &villagers);
+void print(map<string, tuple<int, string, string>> &villagers);
 
 int main() {
     //       <friendship, species, catchphrase>
     map<string, tuple<int, string, string>> villagers;
 
-    int choice = menu();
+    bool quit = false;
+    while (!quit){
+        int choice = menu();
+        switch (choice){
+            case 1:
+                add_vil(villagers);
+                break;
+            case 2:
+                delete_vil(villagers);
+                break;
+            case 3:
+                increase(villagers);
+                break;
+            case 4:
+                decrease(villagers);
+                break;
+            case 5:
+                search(villagers);
+                break;
+            case 6:
+                quit = true;
+                break;
+        }
+    }
 
     return 0;
 }
@@ -29,6 +59,31 @@ int menu(){
     cout << "Enter your choice: ";
     
     int choice; cin >> choice;
-
+    if (choice < 1 || choice > 6){ // Input Validation
+        cout << "\n<Invalid Input>\n";
+        return menu();
+    }
+    
+    cout << endl;
     return choice;
+}
+
+void add_vil(map<string, tuple<int, string, string>> &villagers){
+    cout << "Name: ";
+    string name; getline(cin, name);
+}
+
+void delete_vil(map<string, tuple<int, string, string>> &villagers){
+}
+
+void increase(map<string, tuple<int, string, string>> &villagers){
+}
+
+void decrease(map<string, tuple<int, string, string>> &villagers){
+}
+
+void search(map<string, tuple<int, string, string>> &villagers){
+}
+
+void print(map<string, tuple<int, string, string>> &villagers){
 }
